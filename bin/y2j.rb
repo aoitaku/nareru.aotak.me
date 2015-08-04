b673f64ba955288f3ff4b6679f5230e5ea5dadeb
aotak <aoitaku@gmail.com>
2015-08-04 17:56:19 +0900

    ファイル名の指定が間違っているので修正

--- bin/y2j.rb
+++ bin/y2j.rb
@@ -2,4 +2,4 @@ require 'pathname'
 require 'yaml'
 require 'oj'
 file = (Pathname(__FILE__).dirname / "../data/tweets").to_s
-File.write("#{file}.json", Oj.dump(YAML.load_file("#{file}.yaml")))
+File.write("#{file}.json", Oj.dump(YAML.load_file("#{file}.yml")))
